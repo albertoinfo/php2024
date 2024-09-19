aa57a0890c0a2f1d78a4c533f0d22005b3df640c
alberto <alberto.lopez@iestetuan.es>
2024-09-19 10:41:04 +0200

    Primer ejemplo

---
+++ ejemplos01/test01.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>Ejemplo PHP 01</title>
+</head>
+
+<body>
+  <h1>EJEMPLO BÁSICO </H1>
+  <HR>
+  <?php
+  echo "<b> INFORMACIÓN SOBRE PHP INSTALADA EN EL SERVIDOR </b> <br>";
+  phpinfo();
+  ?>
+</body>
+</html>
+
